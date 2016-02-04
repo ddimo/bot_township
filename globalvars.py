e7cac03bfa235aaa2bc3f7c2beeaec6640dc3ff5
Dmitriy Molchanov <dim2k@mail.ru>
2016-02-04 22:21:38 +0300

    file

---
+++ globalvars.py
@@ -0,0 +1,10 @@
+from classes import *
+
+buildingSettings = dict()
+gameInfo = gameInfoClass()
+upgradesReqs = dict()
+animalsReqs = dict()
+ratingToLevelup = dict()
+ratingForChest = dict()
+
+f = open ("result.html","w")
